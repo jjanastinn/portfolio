953befff5b0a0a3922099ea80ae2c2da9f86f897
Jana Stinn <jana-stinn@gmx.de>
2018-06-01 22:53:03 +0200

    added fade out effect

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 10, immediate = true) {
   var timeout;
   return function() {
     var context = this, args = arguments;
@@ -23,8 +23,10 @@ const body = document.querySelector('body');
 
 function checkPosition(e) {
 
-    const colorAddStart = container1.offsetHeight + container2.offsetHeight - container3.offsetHeight / 2; //900
-    const colorAddEnd = container1.offsetHeight + container2.offsetHeight - container3.offsetHeight / 4; //1050
+    const colorAddStart = container1.offsetHeight + container2.offsetHeight; 
+    const colorAddEnd = container1.offsetHeight + container2.offsetHeight + container3.offsetHeight / 3;
+    const colorRemoveStart = container1.offsetHeight + container2.offsetHeight + (container3.offsetHeight / 3) * 2; 
+    const colorRemoveEnd = container1.offsetHeight + container2.offsetHeight + container3.offsetHeight;
     const colorSectionHeight = (colorAddEnd - colorAddStart) / 100;
 
     if (window.scrollY > colorAddStart && window.scrollY < colorAddEnd) {
@@ -32,8 +34,11 @@ function checkPosition(e) {
       const opacityVal = currentSectionNumber / 100;
       const colorVal = "rgba(191,14,89," + opacityVal + ")";
       document.body.style.backgroundColor = colorVal; 
-    } else {
-      document.body.style.backgroundColor = "white";
+    } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
+      const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
+      const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
+      const colorValOut = "rgba(191,14,89," + reversedOpacityVal + ")";
+      document.body.style.backgroundColor = colorValOut;
     }
    
 }
