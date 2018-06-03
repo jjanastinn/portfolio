c7df6873d1a4c42b3bf626b9fbad8abe11fbdbd7
Jana Stinn <jana-stinn@gmx.de>
2018-06-03 18:58:42 +0200

    added burger menu template tag

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function debounce(func, wait = 10, immediate = true) {
   };
 };
 
+// BURGER MENU
+
+
+
 
 
 const containersNodeList = document.querySelectorAll('.container');
@@ -26,73 +30,81 @@ const body = document.querySelector('body');
 
 function checkPosition(e) {
 
-  containers.forEach(container => {
-    const index = containers.indexOf(container);
-    let containersBefore = containers.slice(0, index);
-    let colorAddStart = 0;
-    containersBefore.forEach(containerBefore => {
-      colorAddStart += containerBefore.offsetHeight;
-    });
-    const colorAddEnd = colorAddStart + container.offsetHeight / 3;
-    const colorRemoveStart = colorAddStart + (container.offsetHeight / 3) * 2; 
-    const colorRemoveEnd = colorAddStart + container.offsetHeight;
-    const colorSectionHeight = (colorAddEnd - colorAddStart) / 100;
+// // CHANGE BACKGROUND COLOR DEPENDING ON SCROLL POSITION
+//   containers.forEach(container => {
+//     const index = containers.indexOf(container);
+//     let containersBefore = containers.slice(0, index);
+//     let colorAddStart = 0;
+//     containersBefore.forEach(containerBefore => {
+//       colorAddStart += containerBefore.offsetHeight;
+//     });
+//     const colorAddEnd = colorAddStart + container.offsetHeight / 3;
+//     const colorRemoveStart = colorAddStart + (container.offsetHeight / 3) * 2; 
+//     const colorRemoveEnd = colorAddStart + container.offsetHeight;
+//     const colorSectionHeight = (colorAddEnd - colorAddStart) / 100;
 
-    const currentSectionNumber = Math.round((window.scrollY - colorAddStart) / colorSectionHeight);
-    const opacityVal = currentSectionNumber / 100;
+//     const currentSectionNumber = Math.round((window.scrollY - colorAddStart) / colorSectionHeight);
+//     const opacityVal = currentSectionNumber / 100;
     
-    const colorVal2 = "rgba(73,62,82," + opacityVal + ")";
-    const colorVal3 = "rgba(69,12,36," + opacityVal + ")";
+//     const colorVal2 = "rgba(73,62,82," + opacityVal + ")";
+//     const colorVal3 = "rgba(69,12,36," + opacityVal + ")";
 
-    const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
-    const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
+//     const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
+//     const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
 
-    const colorValOut1 = "rgba(1,66,51," + reversedOpacityVal + ")";
-    const colorValOut2 = "rgba(73,62,82," + reversedOpacityVal + ")";
-    const colorValOut3 = "rgba(69,12,36," + reversedOpacityVal + ")";
+//     const colorValOut1 = "rgba(1,66,51," + reversedOpacityVal + ")";
+//     const colorValOut2 = "rgba(73,62,82," + reversedOpacityVal + ")";
+//     const colorValOut3 = "rgba(69,12,36," + reversedOpacityVal + ")";
 
-    let className = container.classList[1];
+//     let className = container.classList[1];
 
-    if (window.scrollY > colorAddStart && window.scrollY < colorAddEnd) {
-      switch (className) {
-        case "container2":
-          document.body.style.backgroundColor = colorVal2;
-          break;
-        case "container3":
-          document.body.style.backgroundColor = colorVal3;
-          break;
-        default:
-          break;
-      }
-    } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
-        switch (className) {
-          case "container1":
-            document.body.style.backgroundColor = colorValOut1;
-            break;
-          case "container2":
-            document.body.style.backgroundColor = colorValOut2;
-            break;
-          case "container3":
-            document.body.style.backgroundColor = colorValOut3;
-            break;
-          default:
-            break;
-        }
-    }
-  })
-
+//     if (window.scrollY > colorAddStart && window.scrollY < colorAddEnd) {
+//       switch (className) {
+//         case "container2":
+//           document.body.style.backgroundColor = colorVal2;
+//           break;
+//         case "container3":
+//           document.body.style.backgroundColor = colorVal3;
+//           break;
+//         default:
+//           break;
+//       }
+//     } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
+//         switch (className) {
+//           case "container1":
+//             document.body.style.backgroundColor = colorValOut1;
+//             break;
+//           case "container2":
+//             document.body.style.backgroundColor = colorValOut2;
+//             break;
+//           case "container3":
+//             document.body.style.backgroundColor = colorValOut3;
+//             break;
+//           default:
+//             break;
+//         }
+//     }
+//   })
+  
+// SLIDE IN PROJECTS
   projects.forEach(project => {
-
+    
     let scrollPos = 0;
     const startPosition = window.scrollY + window.innerHeight >= project.offsetTop;
-    if (startPosition) {
-      project.classList.add("active");
-    } else if ((project.getBoundingClientRect()).top > scrollPos) {
-      project.classList.remove("active");
-    }
-    scrollPos = (project.getBoundingClientRect()).top;
-  })
+    const endPosition = window.scrollY + window.innerHeight <= project.offsetTop + project.offsetHeight / 3;
+    const scrollUp = window.scrollY > scrollPos;
 
+    if (startPosition) {
+      project.classList.add("slidein");
+    } else if (scrollUp) {
+      project.classList.remove("slidein");
+    } else if (endPosition && !startPosition && scrollUp) {
+      project.classList.add("slideout");
+    }
+    
+    scrollPos = (document.body.getBoundingClientRect()).top;
+  })
+  
 }  
 
 window.addEventListener('scroll', debounce(checkPosition));
