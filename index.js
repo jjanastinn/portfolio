e467a17d5773eb46f3cbb79dd7dc21dd29481160
Jana Stinn <jana-stinn@gmx.de>
2018-06-03 14:47:21 +0200

    added animation to project cards

--- index.js
+++ index.js
@@ -15,15 +15,17 @@ function debounce(func, wait = 10, immediate = true) {
   };
 };
 
-// ADD DIFFERENT COLORS FOR DIFFERENT CONTAINERS
+
 
 const containersNodeList = document.querySelectorAll('.container');
-const parallaxBg = document.querySelector('.projectOneBgOne');
+const projectsNodeList = document.querySelectorAll('.project');
 let containers = Array.from(containersNodeList);
+let projects = Array.from(projectsNodeList);
 
 const body = document.querySelector('body');
 
 function checkPosition(e) {
+
   containers.forEach(container => {
     const index = containers.indexOf(container);
     let containersBefore = containers.slice(0, index);
@@ -39,14 +41,13 @@ function checkPosition(e) {
     const currentSectionNumber = Math.round((window.scrollY - colorAddStart) / colorSectionHeight);
     const opacityVal = currentSectionNumber / 100;
     
-    const colorVal1 = "rgba(191,14,89," + opacityVal + ")";
     const colorVal2 = "rgba(73,62,82," + opacityVal + ")";
     const colorVal3 = "rgba(69,12,36," + opacityVal + ")";
 
     const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
     const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
 
-    const colorValOut1 = "rgba(191,14,89," + reversedOpacityVal + ")";
+    const colorValOut1 = "rgba(1,66,51," + reversedOpacityVal + ")";
     const colorValOut2 = "rgba(73,62,82," + reversedOpacityVal + ")";
     const colorValOut3 = "rgba(69,12,36," + reversedOpacityVal + ")";
 
@@ -54,18 +55,13 @@ function checkPosition(e) {
 
     if (window.scrollY > colorAddStart && window.scrollY < colorAddEnd) {
       switch (className) {
-        case "container1":
-          document.body.style.backgroundColor = colorVal1;
-          break;
         case "container2":
           document.body.style.backgroundColor = colorVal2;
           break;
         case "container3":
           document.body.style.backgroundColor = colorVal3;
-          parallaxBg.style.backgroundPositionY=  (window.scrollY - parallaxBg.offsetTop) / 1.5+ "px";
           break;
         default:
-          document.body.style.backgroundColor = "white";
           break;
       }
     } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
@@ -80,64 +76,24 @@ function checkPosition(e) {
             document.body.style.backgroundColor = colorValOut3;
             break;
           default:
-            document.body.style.backgroundColor = "white";
             break;
         }
     }
   })
+
+  projects.forEach(project => {
+
+    let scrollPos = 0;
+    const startPosition = window.scrollY + window.innerHeight >= project.offsetTop;
+    if (startPosition && (document.body.getBoundingClientRect()).top < scrollPos) {
+      project.classList.add("active");
+      scrollPos = (document.body.getBoundingClientRect()).top;
+    } else if ((document.body.getBoundingClientRect()).top > scrollPos) {
+      project.classList.remove("active");
+      scrollPos = (document.body.getBoundingClientRect()).top;
+    }
+  })
+
 }  
-
-// ADD ONE COLOR FOR ONE CONTAINER
-
-// const container1 = document.querySelector('.container1');
-// const container2 = document.querySelector('.container2');
-// const container3 = document.querySelector('.container3');
-
-// const body = document.querySelector('body');
-
-// function checkPosition(e) {
-//   const colorAddStart = container1.offsetHeight + container2.offsetHeight; 
-//   const colorAddEnd = container1.offsetHeight + container2.offsetHeight + container3.offsetHeight / 3;
-//   const colorRemoveStart = container1.offsetHeight + container2.offsetHeight + (container3.offsetHeight / 3) * 2; 
-//   const colorRemoveEnd = container1.offsetHeight + container2.offsetHeight + container3.offsetHeight;
-//   const colorSectionHeight = (colorAddEnd - colorAddStart) / 100;
-//   if (window.scrollY > colorAddStart && window.scrollY < colorAddEnd) {
-//     const currentSectionNumber = Math.round((window.scrollY - colorAddStart) / colorSectionHeight);
-//     const opacityVal = currentSectionNumber / 100;
-//     const colorVal = "rgba(191,14,89," + opacityVal + ")";
-//     document.body.style.backgroundColor = colorVal; 
-//   } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
-//     const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
-//     const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
-//     const colorValOut = "rgba(191,14,89," + reversedOpacityVal + ")";
-//     document.body.style.backgroundColor = colorValOut;
-//   }
-// }
-
-// ADD SAME COLOR TO EVERY CONTAINER
-
-// const containers = document.querySelectorAll('.container');
-// const body = document.querySelector('body');
-
-// function checkPosition(e) {
-//   containers.forEach(container => { 
-//     const colorAddStart = container.offsetTop; 
-//     const colorAddEnd = container.offsetTop + container.offsetHeight / 3;
-//     const colorRemoveStart = container.offsetTop + (container.offsetHeight / 3) * 2; 
-//     const colorRemoveEnd = container.offsetTop + container.offsetHeight;
-//     const colorSectionHeight = ((colorAddEnd) - colorAddStart) / 100;      
-//     if (window.scrollY >= colorAddStart && window.scrollY <= colorAddEnd) {
-//       const currentSectionNumber = Math.round((window.scrollY - container.offsetTop) / colorSectionHeight);
-//       const opacityVal = currentSectionNumber / 100;
-//       const colorVal = "rgba(191,14,89," + opacityVal + ")";
-//       document.body.style.backgroundColor = colorVal; 
-//     } else if (window.scrollY > colorRemoveStart && window.scrollY < colorRemoveEnd) {
-//       const currentSectionNumberOut = Math.round((window.scrollY - colorRemoveStart) / colorSectionHeight);
-//       const reversedOpacityVal = (100 - currentSectionNumberOut) / 100;
-//       const colorValOut = "rgba(191,14,89," + reversedOpacityVal + ")";
-//       document.body.style.backgroundColor = colorValOut;
-//     }
-//   });
-// }
 
 window.addEventListener('scroll', debounce(checkPosition));
